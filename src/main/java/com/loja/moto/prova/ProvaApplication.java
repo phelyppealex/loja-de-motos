e7762824e439ec7f5a228cca89a8eba471962a71
Phelyppe Álex <phelyppesilva@gmail.com>
2023-06-01 23:06:57 -0300

    Remoção de SS

--- src/main/java/com/loja/moto/prova/ProvaApplication.java
+++ src/main/java/com/loja/moto/prova/ProvaApplication.java
@@ -13,25 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.loja.moto.prova.model.Usuario;
-import com.loja.moto.prova.repository.UsuarioRepository;
-
 @SpringBootApplication
 public class ProvaApplication implements WebMvcConfigurer {
 
-    @Bean
-	CommandLineRunner commandLineRunner(UsuarioRepository usuarioRepository, BCryptPasswordEncoder encoder) {
-		return args -> {
-			List<Usuario> users = new ArrayList<>();
-			Usuario admin = new Usuario(1,"ADMINISTRADOR","012.345.678-10","admin", encoder.encode("admin"),true);
-			Usuario user1 = new Usuario(2,"João","110.001.110.00","user1", encoder.encode( "user1"),false);
-			Usuario user2 = new Usuario(3,"Maria","000.000.111.22","user2",encoder.encode("user2"),false);
-			users.add(admin);
-			users.add(user1);
-			users.add(user2);
-			usuarioRepository.saveAll(users);
-		};
-	}
+    
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaApplication.class, args);
--- src/main/java/com/loja/moto/prova/model/Usuario.java
+++ src/main/java/com/loja/moto/prova/model/Usuario.java
@@ -2,11 +2,6 @@ package com.loja.moto.prova.model;
 
 import java.util.Collection;
 import java.util.Collections;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario implements UserDetails{
+public class Usuario{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,38 +31,5 @@ public class Usuario implements UserDetails{
 
     public void setSenha(String password){
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.isAdmin){
-            return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
-        }else{
-            return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-        }
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
-    }
-
-    @Override
-    public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
     }
 }
